ea34b5a5015abd96d03ced03cc44b06e7def1a11
Antonio Alfonso Gonzalez Rodriguez <alum.agonzalezr.2021@iesalixar.org>
2021-10-04 11:28:08 +0200

    añado ejercicio 5II_12

--- src/main/java/U1/T5II/E5II_12.java
+++ src/main/java/U1/T5II/E5II_12.java
@@ -10,8 +10,33 @@ public class E5II_12 {
     int num1 = teclado.nextInt();
     int num2 = teclado.nextInt();
     int num3 = teclado.nextInt();
-    int primero;
-    int segundo;
-    int tercero;
+
+    if (num1 >= num2 && num1 >= num3) {
+      if (num2 > num3) {
+        System.out.println(
+            "Los números ordenados de mayor a menos son " + num1 + " > " + num2 + " > " + num3);
+      } else {
+        System.out.println(
+            "Los números ordenados de mayor a menos son " + num1 + " > " + num3 + " > " + num2);
+      }
+    }
+    if (num2 >= num1 && num2 >= num3) {
+      if (num1 > num3) {
+        System.out.println(
+            "Los números ordenados de mayor a menos son " + num2 + " > " + num1 + " > " + num3);
+      } else {
+        System.out.println(
+            "Los números ordenados de mayor a menos son " + num2 + " > " + num3 + " > " + num1);
+      }
+    }
+    if (num3 >= num1 && num3 >= num2) {
+      if (num1 > num2) {
+        System.out.println(
+            "Los números ordenados de mayor a menos son " + num3 + " > " + num1 + " > " + num2);
+      } else {
+        System.out.println(
+            "Los números ordenados de mayor a menos son " + num3 + " > " + num2 + " > " + num1);
+      }
+    }
   }
 }
