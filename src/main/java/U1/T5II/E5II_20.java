43d939db31830cfa33139c8809cad113360d907c
Antonio Alfonso Gonzalez Rodriguez <alum.agonzalezr@iesalixar.org>
2021-10-06 18:15:37 +0200

    añado ejercicio 5II20 sin terminar

--- src/main/java/U1/T5II/E5II_20.java
+++ src/main/java/U1/T5II/E5II_20.java
@@ -1,5 +1,7 @@
 package U1.T5II;
 
+import java.util.Scanner;
+
 public class E5II_20 {
   public static void main(String[] args) {
     // Escribe un programa que calcule el precio final de un producto según su base imponible
@@ -9,6 +11,38 @@ public class E5II_20 {
     // significan respectivamente que no se aplica promoción, el precio se reduce a la mitad, se
     // descuentan 5 euros o se descuenta el 5%. El ejercicio se da por bueno si se muestran los
     // valores correctos, aunque los números no estén tabulados.
+    Scanner teclado = new Scanner(System.in);
+    System.out.println("Introduzca la base imponible");
+    double baseimponible = teclado.nextDouble();
 
+    System.out.println("Introduzca el tipo de IVA (general, reducido o superreducido):");
+    String tipoiva = teclado.next();
+
+    System.out.println("Introduzca el código promocional (nopro, mitad, meno5 o 5porc):");
+    String codpromo = teclado.next();
+
+    if (codpromo.equals("nopro")) {
+      if (tipoiva.equals("general")) {
+        System.out.println("Base imponible: " + baseimponible);
+        System.out.println("IVA (21%): " + baseimponible * 0.21);
+        System.out.println("Precio con IVA: " + baseimponible * 1.21);
+        System.out.println("Cód.promo. (nopro)");
+        System.out.println("Total: " + baseimponible * 1.21);
+      } else if (tipoiva.equals("reducido")) {
+        System.out.println("Base imponible: " + baseimponible);
+        System.out.println("IVA (10%): " + baseimponible * 0.1);
+        System.out.println("Precio con IVA: " + baseimponible * 1.1);
+        System.out.println("Cód.promo. (nopro)");
+        System.out.println("Total: " + baseimponible * 1.1);
+      } else if (tipoiva.equals("superreducido")) {
+        System.out.println("Base imponible: " + baseimponible);
+        System.out.println("IVA (4%): " + baseimponible * 0.04);
+        System.out.println("Precio con IVA: " + baseimponible * 1.04);
+        System.out.println("Cód.promo. (nopro)");
+        System.out.println("Total: " + baseimponible * 1.04);
+      } else {
+        System.out.println("Tipo de iva mal introducido");
+      }
+    }
   }
 }
