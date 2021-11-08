60e3b82c199455d46f17484909b4705664ded338
Antonio Alfonso Gonzalez Rodriguez <alum.agonzalezr@iesalixar.org>
2021-11-08 19:37:38 +0100

    corrigo Ej3 examen sin terminar

--- src/main/java/U1/U1_EXAMEN/Ej3.java
+++ src/main/java/U1/U1_EXAMEN/Ej3.java
@@ -24,13 +24,29 @@ public class Ej3 {
     while (cociente != 0) {
       resto = (int) cociente % 10;
       cociente = cociente / 10;
-      int aleatorio = (int) (1000 + Math.random() * 8999);
+      int aleatorio = (int) (1000 + Math.random() * 9000);
       int d1 = aleatorio / 1000;
       int d2 = (aleatorio % 1000) / 100;
       int d3 = (aleatorio % 100) / 10;
       int d4 = aleatorio % 10;
-      int sumad = d1 + d2 + d3 + d4;
+      int sumad = d1 + d2 + d3 + d4 + resto;
       resultado = resultado + sumad;
+      System.out.println("La clave criptográfica generada para " + resto + " es: " + aleatorio);
+      System.out.println(
+          "La suma de las cifras totales para "
+              + resto
+              + " es: "
+              + resto
+              + " (cifra) + "
+              + d1
+              + " + "
+              + d2
+              + " + "
+              + d3
+              + " + "
+              + d4
+              + " (clave) = "
+              + sumad);
     }
     System.out.println("El número resultante es: " + resultado);
   }
