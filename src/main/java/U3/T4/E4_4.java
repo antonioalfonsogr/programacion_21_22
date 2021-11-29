514a72dabd269aba4b65b3db8fdc9f344a7968c2
Antonio Alfonso Gonzalez Rodriguez <alum.agonzalezr@iesalixar.org>
2021-11-29 17:25:22 +0100

    añado e4_4 y e4_5

--- src/main/java/U3/T4/E4_4.java
+++ src/main/java/U3/T4/E4_4.java
@@ -1,5 +1,6 @@
 package U3.T4;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class E4_4 {
@@ -11,16 +12,76 @@ public class E4_4 {
 
     Scanner sc = new Scanner(System.in);
 
-    int[][] tablero = new int[8][8];
+    String[][] tablero = new String[8][8];
+    String letras = "abcdefgh";
 
-    System.out.println("Introduzca la posición del alfil:");
-    //   String posicionIntroducida = sc.next();
-
-    int letra = sc.nextInt();
-    int numero = sc.nextInt();
-
-    for (int i = letra; i < 8; i++) {
-      for (int j = numero; j < 8; j++) {}
+    for (int i = 0; i < tablero.length; i++) {
+      for (int j = 0; j < tablero[i].length; j++) {
+        tablero[i][j] = letras.charAt(j) + String.valueOf(tablero.length - i);
+      }
     }
+    for (String[] fila : tablero) {
+      System.out.println(Arrays.toString(fila));
+    }
+
+    System.out.println();
+    System.out.println("Introduzca la posición del alfil:");
+    String posicionIntroducida = sc.next();
+    System.out.println();
+
+    int x = -1;
+    int y = -1;
+
+    for (int i = 0; i < tablero.length; i++) {
+      for (int j = 0; j < tablero[i].length; j++) {
+        if (tablero[i][j].equalsIgnoreCase(posicionIntroducida)) {
+          x = i;
+          y = j;
+        }
+      }
+    }
+
+    int incrementoX;
+    int incrementoY;
+
+    incrementoX = -1;
+    incrementoY = 1;
+    String movimientosPosiblesd1 = movimientos(tablero, x, y, incrementoX, incrementoY);
+
+    incrementoX = -1;
+    incrementoY = -1;
+    String movimientosPosiblesd2 = movimientos(tablero, x, y, incrementoX, incrementoY);
+
+    incrementoX = 1;
+    incrementoY = 1;
+    String movimientosPosiblesd3 = movimientos(tablero, x, y, incrementoX, incrementoY);
+
+    incrementoX = 1;
+    incrementoY = -1;
+    String movimientosPosiblesd4 = movimientos(tablero, x, y, incrementoX, incrementoY);
+
+    String movimientoPosiblesResultado =
+        movimientosPosiblesd1
+            + movimientosPosiblesd2
+            + movimientosPosiblesd3
+            + movimientosPosiblesd4;
+
+    System.out.println(
+        "El alfil puede moverse a las siguientes posiciones: " + movimientoPosiblesResultado);
+  }
+
+  public static String movimientos(
+      String[][] matriz, int x, int y, int incrementoX, int incrementoY) {
+    String movimientos = "";
+    x = x + incrementoX;
+    y = y + incrementoY;
+
+    while (x >= 0 && x < matriz.length && y >= 0 && y < matriz.length) {
+      movimientos += matriz[x][y] + " ";
+      x = x + incrementoX;
+      y = y + incrementoY;
+    }
+
+    return movimientos;
   }
 }
