11c6ed0821ae1bd3a50a0bf8839296d708a7ff08
Antonio Alfonso Gonzalez Rodriguez <alum.agonzalezr@iesalixar.org>
2022-03-16 18:49:45 +0100

    16/03

--- src/main/java/U7/T3b/Traductor.java
+++ src/main/java/U7/T3b/Traductor.java
@@ -1,7 +1,6 @@
 package U7.T3b;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.HashMap;
 
 public class Traductor {
@@ -10,24 +9,35 @@ public class Traductor {
 
   public Traductor(String rutaDiccionario) {
 
+    this.diccionario = new HashMap();
+
     try {
-      FileInputStream fichero = new FileInputStream(rutaDiccionario);
-      ObjectInputStream in = new ObjectInputStream(fichero);
-      //      while (true) {
-      //        String linea = (String) in.readObject();
-      //        String[] aux = linea.split("; ");
-      //        diccionario.put(aux[0], aux[1]);
-      //      }
+      BufferedReader in = new BufferedReader(new FileReader(rutaDiccionario));
+      String linea = in.readLine();
+      while (linea != null) {
+        String[] aux = linea.split(", ");
+        diccionario.put(aux[0], aux[1]);
+        linea = in.readLine();
+      }
 
     } catch (IOException ex) {
       System.out.println("Error al leer el fichero");
-      //    } catch (ClassNotFoundException e) {
-      //      e.printStackTrace();
     }
   }
 
   public String traduceString(String fraseEspañol) {
     String fraseIngles = "";
+
+    String[] fraseArrayEspannol = fraseEspañol.split("\\s");
+    String[] fraseArrayIngles = new String[fraseArrayEspannol.length];
+
+    for (int i = 0; i < fraseArrayIngles.length; i++) {
+      fraseArrayIngles[i] = (String) diccionario.get(fraseArrayEspannol[i]);
+    }
+
+    for (int i = 0; i < fraseArrayIngles.length; i++) {
+      fraseIngles += fraseArrayIngles[i] + " ";
+    }
 
     return fraseIngles;
   }
