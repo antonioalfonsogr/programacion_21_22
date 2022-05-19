301a2594a4a2b356943d0a0b46dac26986e69112
Antonio Alfonso Gonzalez Rodriguez <alum.agonzalezr@iesalixar.org>
2022-05-19 18:04:23 +0200

    U9_Entregable

---
+++ src/main/java/U9/U9_Entregable/Principal.java
@@ -0,0 +1,16 @@
+package U9.U9_Entregable;
+
+public class Principal {
+  public static void main(String[] args) {
+
+    Consultas.FiltroPrecio();
+
+    Consultas.InfoPagos();
+
+    Consultas.informeCategoria();
+
+    Transaciones.insertarUnEmpledoYDosClientes();
+
+    Transaciones.insertarOficinaTokyoYTrasladarEmpleados();
+  }
+}
